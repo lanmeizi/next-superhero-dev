75793a119df75ce5be0cf29fa6cfaacd8c72a129
lanmeizi <957801791@qq.com>
2020-05-12 22:53:22 +0800

    初始化项目 项目首页完成

---
+++ components/helloComp.vue
@@ -0,0 +1,31 @@
+<template name="helloComp">
+	<view>
+		{{msg}}
+		<view>
+			<input type="text" :value="myvue" class="txt" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'helloComp',
+		props: {
+			myvue: {
+				type: String,
+				default: ''
+			}
+		},
+		data() {
+			return {
+				msg: '自定义组件'
+			}
+		}
+	}
+</script>
+
+<style>
+	.txt {
+		color: red;
+	}
+</style>
